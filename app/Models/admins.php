dbb08181a34e8429ac891c1b9dc8a9f83dbb48b8
rifqikhuluki <rifkikuluki09@gmail.com>
2025-05-21 12:57:47 +0700

    pembaruan 10

--- app/Models/admins.php
+++ app/Models/admins.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class admins extends Model
 {
-    //
+    protected $table = 'admins';
+
+    protected $primaryKey = 'id_admin';
+
+    protected $fillable = [
+        'id_admin',
+        'email',
+        'password'
+    ];
 }
